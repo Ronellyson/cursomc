bdd38ef147242b4576cf30849a0a4e027edede9e
Ronellyson <ronellysonjulio@gmail.com>
2021-08-15 14:56:49 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/ronellysonjulio/cursomc/services/CategoriaService.java
+++ src/main/java/com/ronellysonjulio/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.ronellysonjulio.cursomc.services;
 
 import com.ronellysonjulio.cursomc.domain.Categoria;
 import com.ronellysonjulio.cursomc.repositories.CategoriaRepository;
+import com.ronellysonjulio.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,8 @@ public class CategoriaService {
 
     public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
 }
