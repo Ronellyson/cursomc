00880a094999cc4d063ec4aaa1abe0813dc8c9c4
Ronellyson <ronellysonjulio@gmail.com>
2021-11-12 16:59:38 -0300

    Estado e cidade

---
+++ src/main/java/com/ronellysonjulio/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.ronellysonjulio.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ronellysonjulio.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
