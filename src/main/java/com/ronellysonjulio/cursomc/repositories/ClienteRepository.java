5fc520942ace35b7d739452c67294c1acff6dbb9
Ronellyson <ronellysonjulio@gmail.com>
2021-11-12 23:00:10 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/ronellysonjulio/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,10 @@
+package com.ronellysonjulio.cursomc.repositories;
+
+import com.ronellysonjulio.cursomc.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
---
+++ src/main/java/com/ronellysonjulio/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.ronellysonjulio.cursomc.repositories;
+
+import com.ronellysonjulio.cursomc.domain.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
